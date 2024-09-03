39e1404b62e89af9c4eb6c1bba27ef0caab37201
Agra Navaratne <agranavaratne@gmail.com>
2024-09-03 18:59:30 +0530

    done?

---
+++ App.js
@@ -0,0 +1,39 @@
+import 'react-native-gesture-handler';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import MainNavigation from './navigation/MainNavigation';
+import { NavigationContainer } from '@react-navigation/native';
+import {useFonts} from 'expo-font';
+import { LogBox } from 'react-native';
+
+
+LogBox.ignoreAllLogs();
+
+export default function App() {
+  const [loaded] = useFonts({
+    'Poppins-Light': require('./assets/fonts/Poppins-Light.ttf'),
+    'Poppins-Medium': require('./assets/fonts/Poppins-Medium.ttf'),
+    'Poppins-Regular': require('./assets/fonts/Poppins-Regular.ttf'),
+    'Poppins-SemiBold': require('./assets/fonts/Poppins-SemiBold.ttf'),
+    'Poppins-Bold': require('./assets/fonts/Poppins-Bold.ttf'),
+    'AveriaSerifLibre-Regular': require('./assets/fonts/AveriaSerifLibre-Regular.ttf'),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+  return (
+    <NavigationContainer>
+      <MainNavigation />
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
